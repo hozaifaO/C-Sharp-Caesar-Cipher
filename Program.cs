7bc218cd4f07b2aa145640518c11fe1e68401e4d
k4b00m-Hozaifa <47463707+k4b00m-Hozaifa@users.noreply.github.com>
2019-11-22 18:40:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Learning_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Word or Name - ");
+            string word = Console.ReadLine();
+            Console.Write("Enter a Shift Key - ");
+            string key_shift = Console.ReadLine();
+            int key = Int32.Parse(key_shift);
+
+            Console.WriteLine(encrypt(key,word));
+
+        }
+
+        public static string encrypt(int key, string word) {
+            char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z'};
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+            word = textInfo.ToUpper(word);
+            string newWord = "";
+            foreach (char c in word)
+            {
+                int i = Array.IndexOf(alphabet, c);
+
+                if (i == 25)
+                {
+
+                    newWord += alphabet[(i-25)+ (key-1)];
+                }
+                else { newWord += alphabet[i + key]; }
+                
+
+            }
+
+            return newWord;
+        }
+    }
+}
